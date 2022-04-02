e2aef989f3224777f503a6c6f4f93a61c9fcbff7
yanghongyang <15652575067@163.com>
2022-04-01 20:05:17 -0700

    temp file: printf for display

---
+++ src/ca/ubc/cs304/controller/TEMP.java
@@ -0,0 +1,16 @@
+package ca.ubc.cs304.controller;
+
+public class TEMP {
+    public static void main(String[] args) {
+        System.out.println("|-------------------------------------------------------------- MENU -----------------------------------------------------------------------------|");
+        System.out.println("| 1. Show all company                            | 2. Show hotels under a specific company           | 16. Find hotels with no available rooms    |");
+        System.out.println("| 15. Find the most expensive room of each hotel | 14. Check all available rooms in a specific hotel | 6. Check Room information                  |");
+        System.out.println("| 11. Update the room status                     | 7. Add a new room                                 |                                            |");
+        System.out.println("| 3. Show workers under a specific department    | 4. Check a worker's work type(part-time/full-time)| 10. Add a new worker                       |");
+        System.out.println("| 12. Delete a worker                            | 9. Check all customers of a specific hotel        | 5. Check whether a customer is a membership|");
+        System.out.println("| 8. Check a customers' bills                    | 13. Add a customer                                |                                            |");
+        System.out.println("| 17. Quit                                       |                                                   |                                            |");
+        System.out.println("|-------------------------------------------------------------------------------------------------------------------------------------------------|");
+        System.out.print("Please choose one of the above 17 options: ");
+    }
+}
